e4986b842a88f05d263ebf34da91a705cf4636b2
Alexey Slivka <slivka77@inbox.ru>
2016-03-01 21:49:12 +0300

    Включена проверка pundit after_action :verify_authorized

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,5 @@ class ApplicationController < ActionController::Base
   include Pundit
   before_action :authenticate_user!
   protect_from_forgery with: :exception
+  after_action :verify_authorized
 end
