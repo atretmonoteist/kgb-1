a198823ff31d744d2acbe1cfbb7935b23cd49e69
atilla777 <slivka77@inbox.ru>
2016-03-11 15:40:36 +0300

    Изменен способ фильтрации доступных для просмотра работ.

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -11,8 +11,13 @@ class JobsController < ApplicationController
   # GET /jobs.json
   def index
     authorize Job
-    #@jobs = Job.all
-    @jobs = policy_scope(Job)
+    #
+    if current_user.has_any_role? :admin, :editor, :viewer
+      @jobs = Job.all
+    else
+      organizations_ids = policy_scope(Organization).pluck(:id)
+      @jobs = Job.where("organization_id IN (#{organizations_ids.join(', ')})").load
+    end
   end
 
   # GET /jobs/1
