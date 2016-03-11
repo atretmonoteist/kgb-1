c379c15d81ce6b267d6f9548d86880398b37305f
atilla777 <slivka77@inbox.ru>
2016-03-11 15:42:50 +0300

    Изменен способ фильтрации доступных для просмотра работ.

--- app/policies/job_policy.rb
+++ app/policies/job_policy.rb
@@ -2,17 +2,23 @@ class JobPolicy < ApplicationPolicy
 
   class Scope < Scope
     def resolve
-      if @user.has_any_role? :admin, :editor, :viewer
-        scope.all
-      else
-        scope.with_roles(@user.roles.map{|role| role.name.to_sym }, @user)
-      end
+      scope
     end
   end
 
   def index?
-    if @user.has_any_role? :local_editor, :local_viewer
+    if @user.has_any_role? :admin, :editor, :viewer, :local_editor, :local_viewer
       true
+    else
+      false
+    end
+  end
+
+  def show?
+    if scope.where(:id => record.id).exists?
+      true
+    elsif
+      @user.has_any_role? :admin, :editor, :viewer, :local_editor, :local_viewer
     else
       false
     end
